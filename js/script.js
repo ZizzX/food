5690afb3d008408c9d6e0bbfcd52d8596655d3a8
Aziz <zizzxiii@gmail.com>
2020-12-31 12:39:27 +0600

    js tabs

--- js/script.js
+++ js/script.js
@@ -93,7 +93,45 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
+window.addEventListener('DOMContentLoaded', () => {
+  const tabs = document.querySelectorAll('.tabheader__item');
+  const tabContent = document.querySelectorAll('.tabcontent');
+  const tabConteiner = document.querySelector('.tabheader__items');
 
+  function hideContent() {
+    tabContent.forEach(item => {
+      item.classList.add('hide');
+      item.classList.remove('show', 'fade');
+    });
+    tabs.forEach(item => {
+      item.classList.remove('tabheader__item_active');
+    });
+  }
+
+  ;
+
+  function showContent(i = 0) {
+    tabContent[i].classList.add('show', 'fade');
+    tabContent[i].classList.remove('hide');
+    tabs[i].classList.add('tabheader__item_active');
+  }
+
+  ;
+  tabConteiner.addEventListener('click', function (event) {
+    const target = event.target;
+
+    if (target && target.classList.contains('tabheader__item')) {
+      tabs.forEach((item, i) => {
+        if (target == item) {
+          hideContent();
+          showContent(i);
+        }
+      });
+    }
+  });
+  hideContent();
+  showContent();
+});
 
 /***/ })
 
